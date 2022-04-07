4147d594f71041e0484eababd4891920ff523418
raychannudam <channudamray2002.rcu@gmail.com>
2022-04-07 08:42:43 +0700

    start-new-structure

---
+++ JavaFx-Ecommerce/src/DatabaseConnection.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+
+    public Connection getConnection(String databaseName, String databaseUser, String databasePassword){
+        String url = "jdbc:mysql://localhost/"+databaseName;
+        try {
+            Class.forName("come.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return databaseLink;
+    }
+}
